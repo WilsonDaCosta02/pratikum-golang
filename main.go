fc644d6df0dccd0a19feb93485019b1f6732a18c
wilsondacosta0205 <dacostawillybrodus@gmail.com>
2025-03-21 03:21:37 +0800

    melakukan instalasi library gin dengan perintah  go get -u github.com/gin-gonic/gin dan mengubah isi di file main.go

--- main.go
+++ main.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-        "encoding/json"
-        "fmt"
-        "net/http"
-        "time"
+	"fmt"
+	"net/http"
+	"time"
 
-        "gorm.io/driver/mysql"
-        "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type User struct {
-        ID        uint      `gorm:"column:id;primaryKey"`
-        Name      string    `gorm:"column:name"`
-        Email     string    `gorm:"column:email"`
-        Age       string    `gorm:"column:age"`
-        CreatedAt time.Time `gorm:"column:createdAt"`
-        UpdatedAt time.Time `gorm:"column:updatedAt"`
+	ID        uint      `gorm:"column:id;primaryKey"`
+	Name      string    `gorm:"column:name"`
+	Email     string    `gorm:"column:email"`
+	Age       string    `gorm:"column:age"`
+	CreatedAt time.Time `gorm:"column:createdAt"`
+	UpdatedAt time.Time `gorm:"column:updatedAt"`
 }
 
 func main() {
-        dsn := "root:@tcp(localhost:3306)/openapi?charset=utf8mb4&parseTime=True&loc=Local"
-        db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-        if err != nil {
-                fmt.Println("failed to connect database")
-        }
+	dsn := "root:@tcp(localhost:3306)/openapi?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect database")
+	}
 
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                var result User
-                db.First(&result)
-                response, _ := json.Marshal(result)
-                fmt.Fprintf(w, string(response))
-        })
+	router := gin.Default()
 
-        http.ListenAndServe(":3000", nil)
+	router.GET("/users", func(c *gin.Context) {
+		var users []User
+		db.Find(&users)
+		c.JSON(http.StatusOK, gin.H{"data": users})
+	})
+
+	router.Run(":3000")
 }
